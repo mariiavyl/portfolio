c542c2c5eccb07e848f9ec3c5820e792092a1c8e
Mariia Vylegzhanina <mariia.vylegzhanina@tuni.fi>
2024-10-06 23:15:47 +0300

    v2

--- scripts.js
+++ scripts.js
@@ -1,32 +1,32 @@
-function showPage(pageId) {
-    const sections = document.querySelectorAll('main section');
-    sections.forEach(section => {
-        section.style.display = 'none';
-    });
-
-    const activeSection = document.getElementById(pageId);
-    if (activeSection) {
-        activeSection.style.display = 'block';
-    }
+// Функция для копирования адреса электронной почты в буфер обмена
+function copyEmail() {
+    const email = "marivvyl@gmail.com"; // Ваш адрес электронной почты
+    const tempInput = document.createElement("input"); // Создаем временное поле ввода
+    document.body.appendChild(tempInput); // Добавляем его в документ
+    tempInput.value = email; // Записываем адрес электронной почты в поле
+    tempInput.select(); // Выделяем текст в поле
+    tempInput.setSelectionRange(0, 99999); // Для мобильных устройств
+    document.execCommand("copy"); // Копируем выделенный текст
+    document.body.removeChild(tempInput); // Удаляем временное поле
+    alert("Email address copied to clipboard: " + email); // Сообщаем об успешном копировании
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    showPage('about');
-});
-
+// Ждем, когда весь документ будет загружен
 document.addEventListener('DOMContentLoaded', function () {
-   
-    const backToTopButton = document.getElementById('back-to-top');
+    const backToTopButton = document.getElementById('back-to-top'); // Находим кнопку "Наверх"
 
+
+    // Проверяем прокрутку страницы
     window.onscroll = function () {
         if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-            backToTopButton.style.display = 'block';
+            backToTopButton.style.display = 'block'; // Показываем кнопку, если прокручено более 100px
         } else {
-            backToTopButton.style.display = 'none';
+            backToTopButton.style.display = 'none'; // Скрываем кнопку, если меньше
         }
     };
 
+    // Обработчик клика на кнопку "Наверх"
     backToTopButton.addEventListener('click', function () {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
+        window.scrollTo({ top: 0, behavior: 'smooth' }); // Прокручиваем плавно к верхней части страницы
     });
 });
